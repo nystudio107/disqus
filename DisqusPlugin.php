5385d238c6e778beb2a5e6c39335e4e3a2da3a2a
Andrew Welch <andrew@megalomaniac.com>
2015-11-23 14:18:14 -0500

    Tabs -> spaces

--- DisqusPlugin.php
+++ DisqusPlugin.php
@@ -23,10 +23,10 @@ class DisqusPlugin extends BasePlugin
         return 'https://github.com/khalwat/disqus/blob/master/releases.json';
     }
     
-	public function getVersion()
-	{
-	    return '1.0.1';
-	}
+    public function getVersion()
+    {
+        return '1.0.1';
+    }
 
     public function getSchemaVersion()
     {
@@ -55,29 +55,29 @@ class DisqusPlugin extends BasePlugin
         return new DisqusTwigExtension();
     }
 
-	protected function defineSettings()
-	{
-		return array(
-			'disqusShortname' => array(AttributeType::String, 'label' => 'Disqus Site Short Name', 'default' => ''),
-			'useSSO' => array(AttributeType::Bool, 'label' => 'Use Single Sign On', 'default' => false),
-			'disqusPublicKey' => array(AttributeType::String, 'label' => 'Disqus Public Key', 'default' => ''),
-			'disqusSecretKey' => array(AttributeType::String, 'label' => 'Disqus Secret Key', 'default' => ''),
-			'customLogin' => array(AttributeType::Bool, 'label' => 'Use Custom Login/Logout URLs', 'default' => false),
-			'loginName' => array(AttributeType::String, 'label' => 'name', 'default' => ''),
-			'loginButton' => array(AttributeType::String, 'label' => 'button', 'default' => ''),
-			'loginIcon' => array(AttributeType::String, 'label' => 'icon', 'default' => ''),
-			'loginUrl' => array(AttributeType::String, 'label' => 'url', 'default' => ''),
-			'loginLogoutUrl' => array(AttributeType::String, 'label' => 'logout', 'default' => ''),
-			'loginWidth' => array(AttributeType::String, 'label' => 'width', 'default' => '800'),
-			'loginHeight' => array(AttributeType::String, 'label' => 'height', 'default' => '400'),
-		);
-	}
+    protected function defineSettings()
+    {
+        return array(
+            'disqusShortname' => array(AttributeType::String, 'label' => 'Disqus Site Short Name', 'default' => ''),
+            'useSSO' => array(AttributeType::Bool, 'label' => 'Use Single Sign On', 'default' => false),
+            'disqusPublicKey' => array(AttributeType::String, 'label' => 'Disqus Public Key', 'default' => ''),
+            'disqusSecretKey' => array(AttributeType::String, 'label' => 'Disqus Secret Key', 'default' => ''),
+            'customLogin' => array(AttributeType::Bool, 'label' => 'Use Custom Login/Logout URLs', 'default' => false),
+            'loginName' => array(AttributeType::String, 'label' => 'name', 'default' => ''),
+            'loginButton' => array(AttributeType::String, 'label' => 'button', 'default' => ''),
+            'loginIcon' => array(AttributeType::String, 'label' => 'icon', 'default' => ''),
+            'loginUrl' => array(AttributeType::String, 'label' => 'url', 'default' => ''),
+            'loginLogoutUrl' => array(AttributeType::String, 'label' => 'logout', 'default' => ''),
+            'loginWidth' => array(AttributeType::String, 'label' => 'width', 'default' => '800'),
+            'loginHeight' => array(AttributeType::String, 'label' => 'height', 'default' => '400'),
+        );
+    }
 
     public function getSettingsHtml()
-	{
+    {
        return craft()->templates->render('disqus/settings', array(
            'settings' => $this->getSettings()
        ));
-	}
+    }
 
 }
